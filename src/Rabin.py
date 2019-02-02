206b0758c6bbedeb55effeb6587bd51b8ae1e6b3
Vadim Kozyrevskii <vadikko2@mail.ru>
2019-02-02 14:38:19 +0300

    update Rabin.py

--- src/Rabin.py
+++ src/Rabin.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def encryption(plaintext, n):
-    plaintext = padding (plaintext)
+    plaintext = padding(plaintext)
     return (plaintext ** 2) * (n-1) % n
 
 
@@ -11,7 +11,7 @@ def padding(plaintext):
     binary_str = bin(plaintext)
     binary_str += bin(int(np.mean([ord(ch) for ch in 'paddingbytes'])))[2:]
 
-    return int (binary_str, 2)
+    return int(binary_str, 2)
 
 
 def decryption(a, p, q):
@@ -20,7 +20,7 @@ def decryption(a, p, q):
     r = pow(a, (p + 1) // 4, p)
     s = pow(a, (q + 1) // 4, q)
 
-    gcd, c, d = prime.egcd (p, q)
+    _, c, d = prime.egcd(p, q)
     x = (r * d * q + s * c * p) % n
     y = (r * d * q - s * c * p) % n
     lst = [x, n - x, y, n - y]
@@ -36,6 +36,6 @@ def choose(lst):
         padding = bin(int(np.mean([ord(ch) for ch in 'paddingbytes'])))[2:]
 
         if binary[-len(padding):] == padding:
-            return int(binary[:-len(padding)],2)
+            return int(binary[:-len(padding)], 2)
 
     return ord('?')
